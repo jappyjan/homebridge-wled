6ba678765ba6de9b28757a04db9474e457ec33af
Jan Jaap <mail@janjaap.de>
2020-12-26 13:36:02 +0100

    publishing

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
-const HttpIrTvPlugin_1 = require("./HttpIrTvPlugin");
+const Plugin_1 = require("./Plugin");
 const settings_1 = require("./settings");
 module.exports = (api) => {
-    api.registerPlatform('homebridge-' + settings_1.PLATFORM_NAME, settings_1.PLATFORM_NAME, HttpIrTvPlugin_1.HttpIrTvPlugin);
+    api.registerPlatform('homebridge-' + settings_1.PLATFORM_NAME, settings_1.PLATFORM_NAME, Plugin_1.Plugin);
 };
 //# sourceMappingURL=index.js.map
--- dist/settings.js
+++ dist/settings.js
@@ -4,9 +4,9 @@ exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-exports.PLATFORM_NAME = 'http-ir-tv';
+exports.PLATFORM_NAME = 'wled';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-exports.PLUGIN_NAME = '@jappyjan/homebridge-http-ir-tv';
+exports.PLUGIN_NAME = '@jappyjan/homebridge-wled';
 //# sourceMappingURL=settings.js.map
