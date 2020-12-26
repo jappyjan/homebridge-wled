bc9beda5c011b7a537b00d20fee40f13246607d9
Jan Jaap <mail@janjaap.de>
2020-12-26 13:34:33 +0100

    publishing

---
+++ dist/Accessory.d.ts
@@ -0,0 +1,26 @@
+import { CharacteristicGetCallback, CharacteristicSetCallback, CharacteristicValue, PlatformAccessory } from 'homebridge';
+import { Plugin } from './Plugin';
+export interface Device {
+    'name': string;
+    'ip': string;
+    'port': number;
+}
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export declare class Accessory {
+    private readonly platform;
+    private readonly accessory;
+    private speakerService?;
+    private device;
+    private axios;
+    private state;
+    constructor(platform: Plugin, accessory: PlatformAccessory);
+    configureSpeakerService(): void;
+    setPower(value: CharacteristicValue, callback: CharacteristicSetCallback): Promise<void>;
+    getPower(callback: CharacteristicGetCallback): Promise<void>;
+    setVolume(value: CharacteristicValue, callback: CharacteristicSetCallback): Promise<void>;
+}
+//# sourceMappingURL=Accessory.d.ts.map
---
+++ dist/Accessory.js
@@ -0,0 +1,85 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Accessory = void 0;
+const axios_1 = __importDefault(require("axios"));
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+class Accessory {
+    constructor(platform, accessory) {
+        this.platform = platform;
+        this.accessory = accessory;
+        this.state = {
+            mute: false,
+        };
+        accessory.category = 5 /* LIGHTBULB */;
+        this.device = accessory.context.device;
+        this.axios = axios_1.default.create({
+            baseURL: `${this.device.ip}:${this.device.port}/json`,
+        });
+        this.configureSpeakerService();
+    }
+    configureSpeakerService() {
+        this.platform.log.debug('Adding speaker service');
+        this.speakerService =
+            this.accessory.getService(this.platform.Service.Speaker) ||
+                this.accessory.addService(this.platform.Service.Speaker);
+        // set the volume control type
+        this.speakerService
+            .setCharacteristic(this.platform.Characteristic.Active, this.platform.Characteristic.Active.ACTIVE)
+            .setCharacteristic(this.platform.Characteristic.VolumeControlType, this.platform.Characteristic.VolumeControlType.ABSOLUTE);
+        this.speakerService
+            .getCharacteristic(this.platform.Characteristic.Active)
+            .on('set', this.setPower.bind(this))
+            .on('get', this.getPower.bind(this));
+        this.speakerService
+            .getCharacteristic(this.platform.Characteristic.VolumeSelector)
+            .on('set', this.setVolume.bind(this));
+    }
+    async setPower(value, callback) {
+        this.platform.log.debug('setPower called with: ' + value);
+        try {
+            await this.axios.post('', {
+                on: value,
+            });
+            this.state.mute = !this.state.mute;
+            callback(null);
+        }
+        catch (e) {
+            this.platform.log.debug(e);
+            callback(e);
+        }
+    }
+    async getPower(callback) {
+        this.platform.log.debug('getPower called');
+        try {
+            const response = await this.axios.get('');
+            callback(null, response.data.state.on);
+        }
+        catch (e) {
+            this.platform.log.debug(e);
+            callback(e);
+        }
+    }
+    async setVolume(value, callback) {
+        const brightness = 255 * (100 / value);
+        this.platform.log.debug(`setVolume called with: ${value}, calculated bri: ${brightness}`);
+        try {
+            await this.axios.post('', {
+                bri: brightness,
+            });
+            callback(null);
+        }
+        catch (e) {
+            this.platform.log.debug(e);
+            callback(e);
+        }
+    }
+}
+exports.Accessory = Accessory;
+//# sourceMappingURL=Accessory.js.map
